126d29b24a12c527cc9e623f684f02237ff32950
initdc <initd@outlook.com>
2023-06-02 19:08:36 +0800

    chore: add LED pin define

--- ra_cfg/fsp_cfg/bsp/bsp_pin_cfg.h
+++ ra_cfg/fsp_cfg/bsp/bsp_pin_cfg.h
@@ -2,6 +2,8 @@
 #ifndef BSP_PIN_CFG_H_
 #define BSP_PIN_CFG_H_
 #include "r_ioport.h"
+#define LED1 (BSP_IO_PORT_02_PIN_09)
+#define LED2 (BSP_IO_PORT_02_PIN_10)
 extern const ioport_cfg_t g_bsp_pin_cfg; /* R7FA6M3AH3CFB.pincfg */
 
 void BSP_PinConfigSecurityInit();
--- ra_gen/pin_data.c
+++ ra_gen/pin_data.c
@@ -17,6 +17,14 @@ const ioport_pin_cfg_t g_bsp_pin_cfg_data[] = {
         .pin_cfg = ((uint32_t) IOPORT_CFG_PERIPHERAL_PIN | (uint32_t) IOPORT_PERIPHERAL_DEBUG)
     },
     {
+        .pin = BSP_IO_PORT_02_PIN_09,
+        .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT | (uint32_t) IOPORT_CFG_PORT_OUTPUT_LOW)
+    },
+    {
+        .pin = BSP_IO_PORT_02_PIN_10,
+        .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT | (uint32_t) IOPORT_CFG_PORT_OUTPUT_LOW)
+    },
+    {
         .pin = BSP_IO_PORT_03_PIN_00,
         .pin_cfg = ((uint32_t) IOPORT_CFG_PERIPHERAL_PIN | (uint32_t) IOPORT_PERIPHERAL_DEBUG)
     },
